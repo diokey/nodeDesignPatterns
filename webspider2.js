6ebdebe279e67c656758eb168570164a629b2494
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-02-17 22:42:22 -0500

    Implementing webspider2

--- webspider2.js
+++ webspider2.js
@@ -1,4 +1,27 @@
 
+function spiderLinks(currentUrl, body, nesting, callback) {
+  if (nesting === 0) {
+    return process.nextTick(callback);
+  }
+
+  var links = utilities.getPageLinks(currentUrl, body);
+
+  function iterate(index) {
+    if (index === links.length) {
+      return callback();
+    }
+
+    spider(links[index], nesting -1, function(err) {
+      if (err) {
+        return callback(err);
+      }
+      iterate (index + 1);
+    });
+  }
+
+  iterate(0);
+}
+
 function spider(url, nesting, callback) {
   var filename = utilities.urlToFilename(url);
   fs.readFile(filename, 'utf8', function(err, body) {
